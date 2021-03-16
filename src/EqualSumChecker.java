3a02214e1c453768cce8a536c60f1864289e3ea2
unknown <tupazluisgabriel@gmail.com>
2021-03-16 10:57:40 +0800

    Initial Commit

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,12 @@
+public class EqualSumChecker {
+    //Method that checks if the first and second values are equal to the third value after combining
+    public static boolean hasEqualSum(int a, int b, int c){
+        //checks if the first and second values are equal to the third value after combining
+        //If equal then return true
+        if(a + b == c){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
